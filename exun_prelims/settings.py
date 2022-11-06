4b08df2120386a8fa834a1ce7b21989c61f87c79
ArjunS07 <arjun.sharma07@outlook.com>
2022-11-06 21:41:51 +0530

    Config static whitenoise

--- exun_prelims/settings.py
+++ exun_prelims/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "whitenoise.runserver_nostatic",
+
     'tailwind',
     'django_browser_reload'
 
@@ -151,14 +153,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = "static/" # url on server, eg localhost:8000/static/
+
+# which directories to find static files in. collectstatic will collect them all and put them together when deployed. also needed for tailwind
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, "static"),
 ]
+# what to call the compiled static dir
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
