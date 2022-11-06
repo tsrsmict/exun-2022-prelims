816ab74cfac286e87cab3938b5555dbe4738a112
ArjunS07 <arjun.sharma07@outlook.com>
2022-11-06 21:48:14 +0530

    Add tailwind boilerplate

--- exun_prelims/urls.py
+++ exun_prelims/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
