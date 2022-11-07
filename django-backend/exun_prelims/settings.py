54467b9d4a4a5c988fb0d53ca56bbab0cea45684
ArjunS07 <arjun.sharma07@outlook.com>
2022-11-07 20:14:38 +0530

    Token auth

--- django-backend/exun_prelims/settings.py
+++ django-backend/exun_prelims/settings.py
@@ -142,13 +142,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-               'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES':(
-                'rest_framework.permissions.IsAuthenticated',
-    ),
-
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 # Internationalization
