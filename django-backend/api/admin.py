7b91cfee016627b396e0ed97bac4e4b4259952a9
ArjunS07 <arjun.sharma07@outlook.com>
2022-11-07 18:24:53 +0530

    Add NFT collectible model

--- django-backend/api/admin.py
+++ django-backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import NFTCollectible
 # Register your models here.
+@admin.register(NFTCollectible)
+class NFTCollectibleAdmin(admin.ModelAdmin):
+    pass
--- django-backend/api/models.py
+++ django-backend/api/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+import uuid
 
 # Create your models here.
 class NFTCollectible(models.Model):
-    pass
+    token = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    price = models.FloatField()
+    
+    user = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_NULL)
+    @property
+    def is_bought(self):
+        return self.user is not None
+
+    def __str__(self):
+        return self.name
+    
+    def __save__(self, *args, **kwargs):
+        if not self.token: self.token = str(uuid.uuid4())[:12]
+        super(NFTCollectible, self).save(*args, **kwargs)
