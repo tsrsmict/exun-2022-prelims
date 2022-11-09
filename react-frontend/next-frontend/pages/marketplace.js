c0dd2bfc0db6e0b117007529bb9c3e9e33997f1f
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2022-11-09 07:45:50 +0530

    marketplace code done
    Co-authored-by: Shreedhar Raj 6355 [TSRS-ML] <Shreedhar.Raj6355@tsrs.org>

--- react-frontend/next-frontend/pages/marketplace.js
+++ react-frontend/next-frontend/pages/marketplace.js
@@ -1,39 +1,44 @@
 import React from 'react'
-
+import Header from '../components/Header'
 function marketplace() {
   return (
-    <div className="p-12">
+    <div className='bg-black'>
+    <Header/>
+    <div className="p-12 bg-[url('/marketplace.jpeg')] bg-cover bg-repeat-0 backdrop-blur-3xl h-screen w-full">
+        
         <div>
-        <h1 className="text-3xl pt-5">Marketplace</h1>
+        <h1 className="backdrop-blur-2xl text-3xl rounded-2xl text-center font-space text-5xl p-5 font-bold text-white shadow-2xl pb-2">Marketplace</h1>
         </div>
-    <div className="flex">
-    <div class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="inline-flex">
+
+    <div class="m-20 left-20 w-full max-w-sm rounded-lg backdrop-blur-2xl">
         <a href="#">
-            <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image"/>
+            <img class="p-8 w-72 rounded-t-lg" src="/nft1.png" alt="product image"/>
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Exclusive Dumb NFT apple watch</h5>
+                <h5 class="text-xl p-2 font-semibold tracking-tight text-gray-900 dark:text-white">Sonic Engine Traveller</h5>
             </a>
             <div class="flex justify-between items-center">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                <span class="text-3xl font-bold text-gray-900 dark:text-white">5k Coins</span>
+                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy Now</a>
             </div>
         </div>
     </div>
-    <div class="m-20 left-20 w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div class="m-20 left-20 w-full max-w-sm rounded-lg backdrop-blur-2xl">
         <a href="#">
-            <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image"/>
+            <img class="p-8 rounded-t-lg w-72" src="/nft2.png" alt="product image"/>
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Exclusive Dumb NFT apple watch</h5>
+                <h5 class="text-xl font-semibold p-2 tracking-tight text-gray-900 dark:text-white">Space Tripple Cannon Fighter</h5>
             </a>
             <div class="flex justify-between items-center">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                <span class="text-3xl font-bold text-gray-900 dark:text-white">50k Coins</span>
+                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy Now</a>
             </div>
         </div>
+    </div>
     </div>
     </div>
     </div>
